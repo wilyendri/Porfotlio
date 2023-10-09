4c28cbee74de17ec374a719d172cca4db148cba9
wilyendri <wilyendri@gmail.com>
2023-10-09 11:27:58 -0400

    first commit

--- src/main/java/com/wilyendri/portfolio/solarSystemProject/NASAController.java
+++ src/main/java/com/wilyendri/portfolio/solarSystemProject/NASAController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalTime;
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/api")
 public class NASAController {
@@ -18,12 +21,14 @@ public class NASAController {
     public String getEccentricity()  {
         try {
             Double majorSemiAxis = 0.0;
+            LocalTime startTime = LocalTime.now();
             RestTemplate restTemplate = new RestTemplate();
             // Define the URL for the HORIZONS API with the necessary parameters
-            String apiUrl = "https://api.le-systeme-solaire.net/rest/bodies/";
-            String query = "COMMAND='499'"; // 399 represents Mars, you can change it for other celestial bodies
+            String apiUrl = "https://ssd.jpl.nasa.gov/api/horizons.api";
+            String query = "format=json&COMMAND='499'&EPHEM_TYPE=ELEMENTS&CENTER='499'" +
+                    "&OUT_UNITS=AU-D&START_TIME=2022-01-01&STOP_TIME=2022-01-02&STEP_SIZE=1d"; // 499 represents Mars, you can change it for other celestial bodies
             // Make an HTTP GET request to the HORIZONS API and receive the response as a ResponseEntity
-            ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl, String.class);
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl + "?" + query, String.class);
 
             // Parse the response as JSON using Jackson
             ObjectMapper objectMapper = new ObjectMapper();
@@ -32,17 +37,18 @@ public class NASAController {
             // Extract the eccentricity value from the JSON response
 //            String eccentricity = responseEntity.getBody();
 
-            for(JsonNode res : jsonResponse.get("bodies")) {
-                String bodyName = res.get("id").asText();
-
-                if(bodyName.equals("lune")) {
-                    majorSemiAxis = res.get("semimajorAxis").asDouble();
-                }
-            }
+//            for(JsonNode res : jsonResponse.get("bodies")) {
+//                String bodyName = res.get("id").asText();
+//
+//                if(bodyName.equals("lune")) {
+//                    majorSemiAxis = res.get("semimajorAxis").asDouble();
+//                }
+//            }
             System.out.println(jsonResponse.getClass());
-            System.out.println(jsonResponse);
+            System.out.println(startTime);
+            System.out.println(jsonResponse.get("result").asText());
 
-            return "Eccentricity of Mars' orbit: " + majorSemiAxis;
+            return "Eccentricity of Mars' orbit: " + jsonResponse.get("result").asText();
         } catch (Exception e) {
             e.printStackTrace();
             return "Error occurred while fetching data.";
