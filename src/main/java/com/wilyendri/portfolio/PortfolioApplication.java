93772654115d0a75b3f3c84ca4f20cb2d355098d
Wilyendri Duran <wilyendri@Wilyendris-MacBook-Air.local>
2023-12-13 16:37:15 -0500

    Added entityScan to main

--- src/main/java/com/wilyendri/portfolio/PortfolioApplication.java
+++ src/main/java/com/wilyendri/portfolio/PortfolioApplication.java
@@ -2,6 +2,7 @@ package com.wilyendri.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.wilyendri.portfolio.resume")
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
