070987721fee40efc78ba770d219c2879c2605f8
wilyendri <wilyendri@gmail.com>
2023-12-16 12:36:28 -0500

    Removed sensitive info

--- src/main/java/com/wilyendri/portfolio/email/EmailController.java
+++ src/main/java/com/wilyendri/portfolio/email/EmailController.java
@@ -1,7 +1,6 @@
 package com.wilyendri.portfolio.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
