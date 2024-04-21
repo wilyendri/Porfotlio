3482e0c2d3343b7d6af74d865de51d482a1eefc3
wilyendri <wilyendri@gmail.com>
2024-04-21 19:22:14 -0400

    Modified experience description

--- src/main/java/com/wilyendri/portfolio/resume/HomeViewController.java
+++ src/main/java/com/wilyendri/portfolio/resume/HomeViewController.java
@@ -1,9 +1,7 @@
 package com.wilyendri.portfolio.resume;
 
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeViewController {
